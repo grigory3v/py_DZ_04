8b8738d5063a97bac9b01427f0df9e1b09541c6d
Grigory3v <jack977j@gmail.com>
2022-10-04 21:50:18 +0400

    добавил задание 3

--- DZ_03.py
+++ DZ_03.py
@@ -1,34 +1,34 @@
-# 3-Задайте список из вещественных чисел.
-#  Напишите программу, которая найдёт разницу между максимальным
-#   и минимальным значением дробной части элементов.
+# -*- coding: utf-8 -*-
 
+# 3 - В файле, содержащем фамилии студентов и их оценки,
+#  изменить на прописные буквы фамилии тех студентов,
+#  которые имеют средний балл более «4».
+# Нужно перезаписать файл.
 # Пример:
+# Ангела Меркель 5
+# Андрей Валетов 5
+# Фредди Меркури 3
+# Анастасия Пономарева 4
 
-# [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
-# [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
+# Программа выдаст:
+# АНГЕЛА МЕРКЕЛЬ 5
+# АНДРЕЙ ВАЛЕТОВ 5
+# Фредди Меркури 3
+# Анастасия Пономарева 4
+file_new = open("task03_new.txt", "wt", encoding='utf-8')
 
-list_1 = [1.1, 1.2, 3.1, 5.17, 10.02]
-list_2 = [4.07, 5.1, 8.2444, 6.98]
-
-
-def List(incoming_list):
-    """Функция, которая найдёт разницу между максимальным 
-    и минимальным значением дробной части элементов списка."""
-
-    min_value_fractional_part_elements = incoming_list[0] % 1
-    max_value_fractional_part_elements = incoming_list[0] % 1
-
-    for i in range(1, len(incoming_list)):
-
-        if incoming_list[i] % 1 > max_value_fractional_part_elements:
-            max_value_fractional_part_elements = incoming_list[i] % 1
-        elif incoming_list[i] % 1 < min_value_fractional_part_elements:
-            min_value_fractional_part_elements = incoming_list[i] % 1
-
-    difference_between_elements = max_value_fractional_part_elements - \
-        min_value_fractional_part_elements
-    return round(difference_between_elements, 2)
-
-
-print('Разница между максимальным и минимальным значением дробной части элементов list_1 = ', List(list_1))
-print('Разница между максимальным и минимальным значением дробной части элементов list_2 = ', List(list_2))
+with open('task03.txt', encoding='utf-8') as file:
+    for index, line in enumerate(file):
+        for i in range(0, len(line)):
+            if index == i:
+                for i in line:
+                    if i == '5' or i == '4':
+                        new_line = str.upper(line)
+                        file_new.write(new_line)
+                        print(new_line)
+                    elif i == '3' or i == '2':
+                    # else: почему не отрабатывает просто иначе?
+                        file_new.write(line)
+                        print(line)
+                        break
+                break
